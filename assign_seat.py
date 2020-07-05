bd036f0e309f553a5c36237db424af2d41d76dec
root <aloa0302@gmail.com>
2020-07-05 06:36:11 +0000

    test2

--- assign_seat.py
+++ assign_seat.py
@@ -14,25 +14,61 @@ user_list = []
 for row in c.execute('SELECT users.pid, users.full_name, seat.owner_id from users left join seat on users.pid = seat.owner_id where \
                      seat.owner_id is null'):
     user_list.append(row)
-    
-while user_list:
-    a= user_list.pop()
-    b= random.choice(first_pool)
-    first_pool.remove(b) 
-    #a[0],b 활용해서 seat update
-    seat_pool = []
-    for row in c.execute(f'SELECT * from seat where cluster_id = {b} and owner_id is null'):
-        seat_pool.append(row[0])
-    selected_seat = random.choice(seat_pool)
-    
-    c.execute(f'''UPDATE seat SET owner_id ={str(a[0])} WHERE sid={selected_seat}''')
-    
-    c.execute(f'''UPDATE cluster SET number_owned =number_owned +1 WHERE cid ={b}''')
-    
-    if not first_pool:
-        first_pool = []
-        for row in c.execute('SELECT distinct cid from cluster where number_of_seat-number_owned >0'):
-            first_pool.append(*row)
-print('done')
-conn.commit()
-conn.close()
+pid_list = []
+for row in c.execute('SELECT users.pid from users left join seat on users.pid = seat.owner_id where \
+                     seat.owner_id is null'):
+    pid_list.append(*row)
+print('what do you want to do? bulk insert(1), individual insert(2)')
+users_reaction = input()
+if users_reaction =='1':
+    while user_list:
+        a= user_list.pop()
+        b= random.choice(first_pool)
+        first_pool.remove(b) 
+        #a[0],b 활용해서 seat update
+        seat_pool = []
+        for row in c.execute(f'SELECT * from seat where cluster_id = {b} and owner_id is null'):
+            seat_pool.append(row[0])
+        selected_seat = random.choice(seat_pool)
+
+        c.execute(f'''UPDATE seat SET owner_id ={str(a[0])} WHERE sid={selected_seat}''')
+        print(f'assign {a[1]} to seat {selected_seat}')
+
+        c.execute(f'''UPDATE cluster SET number_owned =number_owned +1 WHERE cid ={b}''')
+
+        if not first_pool:
+            first_pool = []
+            for row in c.execute('SELECT distinct cid from cluster where number_of_seat-number_owned >0'):
+                first_pool.append(*row)
+    print('done')
+    conn.commit()
+    conn.close()
+elif users_reaction=='2':
+    first_pool = []
+    for row in c.execute('SELECT distinct cid from cluster where number_of_seat-number_owned >0'):
+        first_pool.append(*row)
+
+    while True:
+        print(pid_list)
+        print('please type pid')
+        mm = input()
+        b= random.choice(first_pool)
+        first_pool.remove(b)
+        seat_pool = []
+        for row in c.execute(f'SELECT * from seat where cluster_id = {b} and owner_id is null'):
+            seat_pool.append(row[0])
+        selected_seat = random.choice(seat_pool)
+        c.execute(f'''UPDATE seat SET owner_id ={str(mm)} WHERE sid={selected_seat}''')
+        print(f'assign {mm} to seat {selected_seat}')
+        c.execute(f'''UPDATE cluster SET number_owned =number_owned +1 WHERE cid ={b}''')
+        conn.commit()
+        if not first_pool:
+            first_pool = []
+            for row in c.execute('SELECT distinct cid from cluster where number_of_seat-number_owned >0'):
+                first_pool.append(*row)
+        print('insert done, do you want to quit? (y/n)')
+        mmm = input()
+        if mmm == 'y':
+            break
+    conn.close()
+        
